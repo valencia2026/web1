54ab58fa08d84975ef1100f0cd5cc0e8454e999b
valencia2026 <jjeanbaptiste41@mail.valenciacollege.edu>
2026-03-08 22:00:12 -0400

    upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function videoFunction() {
+  document.getElementById("myComment").innerHTML = "A great vacation video";
+}
+
+function emailMessage() {
+  document.getElementById("emailBox").innerHTML =
+    "Please enter a valid email address";
+}
+
+function blankMessage() {
+  document.getElementById("emailBox").innerHTML = "Email:";
+}
