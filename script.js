1269bdae03c41b44a241a7c04687cadca63a4395
valencia2026 <jjeanbaptiste41@mail.valenciacollege.edu>
2026-04-26 23:57:21 -0400

    up

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-function videoFunction() {
-  document.getElementById("myComment").innerHTML = "A great vacation video";
-}
+let isEnglish = true;
 
-function emailMessage() {
-  document.getElementById("emailBox").innerHTML =
-    "Please enter a valid email address";
-}
+function toggleLanguage() {
+  isEnglish = !isEnglish;
 
-function blankMessage() {
-  document.getElementById("emailBox").innerHTML = "Email:";
+  document.getElementById("langBtn").innerText = isEnglish ? "ES" : "EN";
+
+  if (!isEnglish) {
+    document.getElementById("title").innerText = "Desarrollador Web";
+    document.getElementById("about").innerText =
+      "Desarrollador apasionado por crear experiencias web modernas.";
+
+    document.getElementById("skillsTitle").innerText = "Habilidades";
+    document.getElementById("projectsTitle").innerText = "Proyectos";
+    document.getElementById("contactTitle").innerText = "Contacto";
+
+    document.getElementById("navHome").innerText = "Inicio";
+    document.getElementById("navProjects").innerText = "Proyectos";
+    document.getElementById("navAbout").innerText = "Sobre mí";
+    document.getElementById("navContact").innerText = "Contacto";
+
+  } else {
+    document.getElementById("title").innerText = "Web Developer";
+    document.getElementById("about").innerText =
+      "Creative Web Developer passionate about modern web experiences.";
+
+    document.getElementById("skillsTitle").innerText = "Skills";
+    document.getElementById("projectsTitle").innerText = "Projects";
+    document.getElementById("contactTitle").innerText = "Contact";
+
+    document.getElementById("navHome").innerText = "Home";
+    document.getElementById("navProjects").innerText = "Projects";
+    document.getElementById("navAbout").innerText = "About";
+    document.getElementById("navContact").innerText = "Contact";
+  }
 }
